2635ee0ffe0b26f8a0769c6c478d342be2701316
Sergey <rumatarin@mail.ru>
2018-11-09 22:44:11 +0300

    HomeWork 10

---
+++ work10/js/js.js
@@ -0,0 +1,6 @@
+// input id="age" value="30">
+let age = document.getElementById('age');
+function showUser(surname, name) {
+	alert("Пользователь " + surname + " " + name + ", его возраст " + this.value);
+}
+showUser.apply(age, ['Johns','Tom'])
